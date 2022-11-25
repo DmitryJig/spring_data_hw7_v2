56c3b4ff4ee348527dec166fa294726cb233e7b6
dima <jd2344430@gmail.com>
2022-11-25 18:38:44 +0500

    master_commit

---
+++ src/main/java/com/example/spring_hw7_v2/SpringHw7V2Application.java
@@ -0,0 +1,13 @@
+package com.example.spring_hw7_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHw7V2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHw7V2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_hw7_v2/SpringHw7V2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_hw7_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHw7V2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
